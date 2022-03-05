c9a56214daeb313c54516192794a0b892b8700fb
don <misurakalu@gmail.com>
2022-03-05 15:58:57 +1000

    More python reading

---
+++ Reading/test2.py
@@ -0,0 +1,11 @@
+import fibo
+
+# importing the modules using the import - save the name of modules
+
+from fibo import fib, fib2 # from fibo import * 
+
+
+# Input/ Output
+# open. read. close.
+
+# Errors and Exceptions
